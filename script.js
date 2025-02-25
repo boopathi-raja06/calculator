b662d4b0a8da41044aefcfbc85fc74fe5747d211
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-02-25 05:38:20 +0530

    with mistake

--- script.js
+++ script.js
@@ -1,18 +1,73 @@
-const but=document.querySelectorAll("button")
-const res=document.querySelector(".result")
-const operator=document.querySelectorAll(".operator")
-const number=document.querySelectorAll(".num")
-let num=[]
-let op=[]
-operator.forEach(ope=>{
-    op.push(ope.textContent)
-})
-number.forEach(n=>{
-    num.push(n.textContent)
-})
+const but = document.querySelectorAll("button");
+const res = document.querySelector(".result");
+const ac = document.querySelector("#ac").textContent;
+const operator = document.querySelectorAll(".operator");
+const number = document.querySelectorAll(".num");
+let num = [];
+let op = [];
+let inp1 = "";
+let inp2 = "";
+let operation = "";
+let result = 0;
+operator.forEach((ope) => {
+  op.push(ope.textContent);
+});
+number.forEach((n) => {
+  num.push(n.textContent);
+});
 
-but.forEach(button=>{
-    button.addEventListener("click",(eve)=>{
-       res.textContent=eve.target.textContent
-    })
-})
+but.forEach((button) => {
+  button.addEventListener("click", (eve) => {
+    let n = eve.target.textContent;
+    //res.textContent=eve.target.textContent
+
+    if(!isNaN(n)||n=='.'){
+        if(operation){
+            res.textContent=''
+            inp2+=n;
+            res.textContent=inp2
+        }
+        else{
+            inp1+=n
+            res.textContent=inp1
+        }
+    }
+    else if(num.includes(n)){
+        if(inp1&&inp2){
+            result= operate(parseFloat(inp1), parseFloat(inp2), operation);
+            res.textContent=result
+            inp1=result.toString()
+        }
+        operation=n
+    }
+    else if (n === "=") {
+        if (inp1 && inp2 && operation) {
+          result = operate(parseFloat(inp1), parseFloat(inp2), operation);
+          res.textContent = result;
+          inp1 = result.toString();
+          inp2 = "";
+          operation = "";
+        }
+    }
+    else if(n==='ac')
+    {
+        inp1 = "";
+        inp2 = "";
+        operation = "";
+        res.textContent = "";
+    }
+  });
+});
+
+function operate(in1, in2, operation) {
+  switch (operation) {
+    case "+":
+      return in1 + in2;
+    case "-":
+      return in1 + in2;
+    case "*":
+      return in1 + in2;
+    case "/":
+      return in1 + in2;
+  }
+}
